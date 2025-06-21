8688f4f24c0004649cbecae7afe2581ec9499605
Mike Curtin <michaelcurtin31@gmail.com>
2025-06-21 17:23:03 -0500

    prompt user to enter city

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def prompt_user_for_city() -> str:
+    """
+    Prompts the user to enter a city name.
+    Returns a titled string stripped of any whitespace characters.
+    """
+    response = str(input("Enter a city: "))
+    return response.title().strip()
+
 def main():
     print("Hello from pdi-weather-app!")
 
