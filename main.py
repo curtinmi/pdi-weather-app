6cf6553732abc3a689184605466c922ccdb5c342
Mike Curtin <michaelcurtin31@gmail.com>
2025-06-23 19:25:32 -0500

    updated function docstrings

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ from dotenv import load_dotenv
 def prompt_user_for_city() -> str:
     """
     Prompts the user to enter a city name.
-    Returns a titled string stripped of any whitespace characters.
     """
     response = str(input("Enter a city: "))
     return response.title().strip()
 
 def call_open_weather_api_city(city: str) -> dict:
     """
-    Accepts a string representing a city location.
     Calls the OpenWeather api and obtains weather data pertaining to that city.
-    Returns the data as a dictionary.
     """
     OPEN_WEATHER_KEY = os.environ['API_KEY']
     api_response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_KEY}')
@@ -23,9 +20,7 @@ def call_open_weather_api_city(city: str) -> dict:
 
 def extract_weather_data(api_data: dict) -> dict:
     """
-    Accepts a (nested) dictionary of api data.
     Extracts relevant values from the supplied dictionary and stores the key/value pair in a new dict.
-    Returns an unnested dictionary.
     """
     weather = {}
     weather['description'] = api_data['weather'][0]['description']
@@ -42,7 +37,6 @@ def extract_weather_data(api_data: dict) -> dict:
 
 def output_weather_stdout(parsed_weather_data: dict, city: str) -> None:
     """
-    Accepts a dictionary of parsed weather data and a string representing the location.
     The weather data for the desired location is printed to the terminal.
     """
     print(f"\n\n{city}'s weather is: ")
