2f5df7a962ff556c53ebeed3675b24932bb487cd
Mike Curtin <michaelcurtin31@gmail.com>
2025-06-24 22:50:40 -0500

    output to display time in local time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime
 
 from typing import NamedTuple
 from dotenv import load_dotenv
@@ -47,6 +48,7 @@ class WeatherData(NamedTuple):
     wind: float
     sunrise: int
     sunset: int
+    timezone_offset: int
 
 def extract_weather_data(api_data: dict) -> WeatherData:
     """
@@ -62,8 +64,17 @@ def extract_weather_data(api_data: dict) -> WeatherData:
         visibility = api_data['visibility'],
         wind = api_data['wind']['speed'],
         sunrise = api_data['sys']['sunrise'],
-        sunset = api_data['sys']['sunset']
+        sunset = api_data['sys']['sunset'],
+        timezone_offset = api_data['timezone']
     )
+
+def unix_time_to_local(unix_time: int) -> datetime:
+    """
+    Converts unix time from API to local time.
+    """
+    dt_obj = datetime.datetime.fromtimestamp(unix_time)
+    return dt_obj.time()
+
 
 def output_weather_stdout(data: WeatherData, city: str) -> None:
     """
@@ -72,14 +83,14 @@ def output_weather_stdout(data: WeatherData, city: str) -> None:
     print(f"\n\n{city}'s weather is: ")
     print(f"description: {data.description}")
     print(f"high temperature: {data.high_temp}")
-    print(f"low temperature: {data.description}")
+    print(f"low temperature: {data.low_temp}")
     print(f"current_temp: {data.current_temp}")
     print(f"feels_like: {data.feels_like}")
     print(f"humidity: {data.humidity}")
     print(f"visibility: {data.visibility}")
     print(f"wind: {data.wind}")
-    print(f"sunrise: {data.sunrise}")
-    print(f"sunset: {data.sunset}")
+    print(f"sunrise: {unix_time_to_local(data.sunrise)}")
+    print(f"sunset: {unix_time_to_local(data.sunset)}")
 
 
 
@@ -87,6 +98,7 @@ def main():
     city = prompt_user_for_city()
     temp_unit = placeholder_unit_prompt()
     api_call = call_open_weather_api_city(city)
+    print(api_call)
     weather_data = extract_weather_data(api_call)
     output_weather_stdout(weather_data, city)
 
